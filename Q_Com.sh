cc9e329b5aff0c832d1b72f16fa2ab1eccc64d11
LanceTreyark <github@treyark.com>
2023-01-26 04:37:49 -0800

    01.26.23 04:37AM

--- Q_Com.sh
+++ Q_Com.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#v.012123
+#v.012623
+date_time="$(date +"%m.%d.%y %I:%M%p")"
 read -p "Please enter your commit message   " var_commit
-echo "* "
-git commit -m "$var_commit"
+echo " "
+git commit -m "$date_time $var_commit"
 echo "*  *"
 echo "*  *  *"
 sleep 1
 echo "Script Complete"
+
