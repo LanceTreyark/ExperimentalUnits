cfc0c2b1c6d45a4af367083a1f7d59b10a7a408c
LanceTreyark <github@treyark.com>
2023-01-26 04:39:15 -0800

    01.26.23 04:39AM

--- Q_Com.sh
+++ Q_Com.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #v.012623
 date_time="$(date +"%m.%d.%y %I:%M%p")"
-read -p "Please enter your commit message   " var_commit
+read -p "(optional) Enter an additional commit message   " var_commit
 echo " "
 git commit -m "$date_time $var_commit"
 echo "*  *"
